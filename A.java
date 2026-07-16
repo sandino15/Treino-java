9ea9f83c78013b0b59fc383e9e6eda9646cf92f8
sandino15 <sandinolee3@gmail.com>
2026-07-16 18:43:18 -0300

    Estudos

---
+++ A.java
@@ -0,0 +1,7 @@
+gh : O termo 'gh' não é reconhecido como nome de cmdlet, função, arquivo de script ou programa operável. Verifique a grafia do nome ou, se um 
+caminho tiver sido incluído, veja se o caminho está correto e tente novamente.
+No linha:1 caractere:1
++ gh auth login
++ ~~
+    + CategoryInfo          : ObjectNotFound: (gh:String) [], CommandNotFoundException
+    + FullyQualifiedErrorId : CommandNotFoundException
